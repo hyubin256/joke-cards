5a4630d61eff38ebe652650b1b171d9e6de6814f
Nguyen Minh Huy <huy.nguyenminh@it-consultis.net>
2023-01-08 21:13:06 +0700

    Updates

---
+++ precache-manifest.4007be8800ea3dcdf62b35d2595a2a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45d4d59b900606275ab8",
+    "url": "./static/css/main.9bfa2412.chunk.css"
+  },
+  {
+    "revision": "45d4d59b900606275ab8",
+    "url": "./static/js/main.45d4d59b.chunk.js"
+  },
+  {
+    "revision": "405e6f42d3a00b4e4100",
+    "url": "./static/js/1.405e6f42.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "86a3c8b4ae2f17a67ed90638a6f456f2",
+    "url": "./static/media/head_banner_background.86a3c8b4.png"
+  },
+  {
+    "revision": "2bc0cdd6d527a556a7264c1a3b594b42",
+    "url": "./static/media/footer_background.2bc0cdd6.png"
+  },
+  {
+    "revision": "767dbcf126bea0df1aad3391e0a822ec",
+    "url": "./index.html"
+  }
+];
